01de2cf8d09c61b73e89ac033137182e511902e8
Adrian Popan <adrian.popan@gmail.com>
2019-06-17 20:11:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
   new_array = []
   i = 0 
   while i < array.length do |element|
-    new_array << yield(element)
+    new_array << yield
+    i = i + 1 
   end
   new_array
 end
